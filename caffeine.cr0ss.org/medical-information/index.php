d400c092b847a796c22f4cbd1b2a1f7340836b6b
Simon Krüger <hallo@cr0ss.org>
2020-08-21 09:33:50 +0200

    Add portfolio template and existing code
    * Adds a custom portfolio template.
    * Adds existing subdomain code for caffeine.

---
+++ caffeine.cr0ss.org/medical-information/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+<link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+</head>
+<body>
+<div>
+<h2>Physical impact</h2>
+<p>
+  The physical data our application gathers from your Smartphone can never substitute the opinion of a trained specialist. See it as a helper to determine the personal limitation for your caffeine consumption, which should not be trespassed. For our calculations we have recourse to a great list of public and open access scientific documents on the caffeine half-life and it’s repercussions on the human body. With this we want to assure that the given advices are as accurately as possible. For users with a significant higher sensibility (e.g. pregnant woman, cardiacs, children) these calculations are inapplicable. Please contact a doctor regarding your recommended daily consumption limits.
+</p>
+</div>
+<br>
+<br>
+<div>
+<h2>Except of Sources</h2>
+<p>
+  The Half Life of Caffeine in healthy adults is about 5.7 hours <a href="http://www.ncbi.nlm.nih.gov/pubmed/7361718?dopt=Abstract">[1]</a>. "However, at higher doses (e.g. 250–500mg single dose), the clearance of caffeine is significantly reduced and its elimination half-life is prolonged, indicating non-linearity" <a href="http://www.militaryenergygum.com/wp-content/uploads/Multiple-Dose-Pharmacokinetics.pdf">[2]</a>.
+</p>
+
+<p>
+  "The t1/2 (beta) was significantly prolonged in women on OCS (10.7 +/- 3.0 hr vs. 6.2 +/- 1.6)" <a href="http://www.ncbi.nlm.nih.gov/pubmed/7359014">[3]</a> unrelated to weight or height of female participants.
+</p>
+</div>
+</body>
+</html>
---
+++ html/README.md
@@ -0,0 +1,3 @@
+# cr0ss.org
+
+This implementation is an adaption of the [ParticleGround-Portfolio](https://github.com/itsron717/ParticleGround-Portfolio) template by itsron717, which is available under the MIT License.
