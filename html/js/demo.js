2b2402d3c29a47c798a1986cb0642e8db10ebbcb
Simon Krüger <hallo@cr0ss.org>
2020-08-21 10:26:29 +0200

    Minor styling updates to fit the color-scheme of the cr0ss.org domain

--- html/js/demo.js
+++ html/js/demo.js
@@ -5,24 +5,19 @@
 
 // This can be used to set the Particles Effects. Check README for more details!
 document.addEventListener('DOMContentLoaded', function () {
-  particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
-  });
+  particleground(
+    document.getElementById('particles'),
+    {
+      dotColor: '#eca139',
+      lineColor: '#eca139',
+      minSpeedX: 0.5,
+      maxSpeedX: 1,
+      minSpeedY: 0.5,
+      maxSpeedY: 1,
+      density: 9000,
+      parallaxMultiplier: 4.5
+    }
+  );
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
 }, false);
-
-
-/*
-// jQuery plugin example:
-$(document).ready(function() {
-  $('#particles').particleground({
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
-  });
-  $('.intro').css({
-    'margin-top': -($('.intro').height() / 2)
-  });
-});
-*/
